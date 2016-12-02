5c7ce2094a1dee8c1d8ea272b3a1cb385c10da58
Plopounet13 <lois@dhcp-176-172.ens-lyon.fr>
2016-12-02 10:16:44 +0100

    Script correction format dossier

--- correct.sh
+++ correct.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
+ls $1 |(while read file; do
+	sed -e "s/;/,/g" -i .tmp $1/$file
+	sed -e "s/,$//g" -i .tmp $1/$file
+	echo "nom,tailleMot,nbResults" > $1/$file.tmp
+	more $1/$file >> $1/$file.tmp
+	cp $1/$file.tmp $1/$file
+	rm $1/$file.tmp
+done)
+
 
