9b79d57905057060ff30b1a2a450c79a2f248be2
Plopounet13 <lois@dhcp-181-95.ens-lyon.fr>
2016-12-13 15:50:08 +0100

    randomChange

--- genRand.py
+++ genRand.py
@@ -1,11 +1,11 @@
 import sys
 from random import *
 
-if len(sys.argv)!=3:
-	print("Erreur: Nombre de parametres incorrect (2 attendu)", file=sys.stderr)
+if len(sys.argv)!=4:
+	print("Erreur: Nombre de parametres incorrect (3 attendu)", file=sys.stderr)
 	exit()
 
 seed(int(sys.argv[1]))
 
-for i in range(1,51):
+for i in range(1,int(sys.argv[3])):
 	print(randint(1, int(sys.argv[2])))
--- recupReq.sh
+++ recupReq.sh
@@ -2,17 +2,21 @@
 
 LANG=C
 
-if [ $# -ne 1 ]; then
-	echo "Erreur nombre de parametres incorrect: nombre requis 1" 2>&1
+if [ $# -ne 2 ]; then
+	echo "Erreur nombre de parametres incorrect: nombre requis 2 (SEED nbMots)" 2>&1
 	exit
 fi
 
 SEED=$1
 RANDOM=$SEED
+
 		
 while read dico; do
 	siz=$(wc -l < $dico)
-	python3 genRand.py $SEED $siz | (while read line;do
+	python3 genRand.py $SEED $siz $2 > randoms.tmp
+	i=0
+	while read line;do
+		i=$(($i+1))
 		mot=$(sed "${line}q;d" $dico)
 		curl -sA "Chrome" -L "http://www.google.com/search?hl=en&q=$mot" -o tmp.html
 		nb=$(grep '<div class="sd" id="resultStats">' < tmp.html | sed -E 's/.*"resultStats">(About )?([1-9][0-9,]*) results.*/\2/' | sed 's/,//g')
@@ -21,8 +25,12 @@ while read dico; do
 			nb=0
 		fi
 		echo $mot\;${#mot}\;$nb\; >> $dico.out
-	done)
-	sleep 60
+		if [ i -e 50 ]; then
+			sleep 30
+			i=0
+		fi
+	done < randoms.tmp
+	sleep 30
 done < listeDico.txt
 
 rm tmp.html
